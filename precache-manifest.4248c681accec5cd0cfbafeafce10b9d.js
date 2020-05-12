203172674559ff3ae9fbc34e9394899c51437457
Zach Dantzer <52724062+Zacison@users.noreply.github.com>
2020-05-12 15:35:10 -0500

    Updates

---
+++ precache-manifest.4248c681accec5cd0cfbafeafce10b9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a02f395c71ac85c33c2792ece614d8e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ac772d60baec92bf7a22",
+    "url": "/monsters-rolodex/static/css/main.adb79342.chunk.css"
+  },
+  {
+    "revision": "cae8900dcf0593a9e8f0",
+    "url": "/monsters-rolodex/static/js/2.975f868f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.975f868f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac772d60baec92bf7a22",
+    "url": "/monsters-rolodex/static/js/main.34468552.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
